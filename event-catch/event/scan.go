199642c45df8db188552824bd3347c9fc80e4558
남승인 <sinam@sds.co.kr>
2024-01-14 12:05:43 +0900

    feat: 루프를 통해 블럭들에서 정보를 확인

--- event-catch/event/scan.go
+++ event-catch/event/scan.go
@@ -1,7 +1,11 @@
 package event
 
 import (
+	"context"
 	"event-catch/config"
+	"fmt"
+	"math/big"
+	"time"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
@@ -24,7 +28,7 @@ func NewScan(config *config.Config, client *ethclient.Client) (*Scan, chan []eth
 
 	eventlog := make(chan []ethType.Log, 100)
 
-	s.lookingScan(config.Node.StartBlock, eventlog)
+	go s.lookingScan(config.Node.StartBlock, eventlog)
 
 	return s, eventlog, nil
 }
@@ -40,6 +44,33 @@ func (s *Scan) lookingScan(
 	s.FilterQuery = ethereum.FilterQuery{
 		Addresses: []common.Address{},
 		Topics:    [][]common.Hash{},
+		FromBlock: big.NewInt(startReadBlock),
 	}
 
+	for {
+		time.Sleep(1e8)
+
+		ctx := context.Background()
+
+		if maxBlock, err := s.client.BlockNumber(ctx); err != nil {
+			fmt.Println("Get BlockNumber", "err", err.Error())
+			continue
+		} else {
+			to = maxBlock
+
+			if to > uint64(startReadBlock) {
+				// s.FilterQuery.FromBlock(startReadBlock)
+				s.FilterQuery.FromBlock = big.NewInt(startReadBlock)
+				s.FilterQuery.ToBlock = big.NewInt(int64(to))
+
+				if logs, err := s.client.FilterLogs(ctx, s.FilterQuery); err != nil {
+					//TODO from, to 블럭만 변형시켜서 다시 호출
+				} else if len(logs) > 0 {
+					eventLog <- logs
+
+					startReadBlock = int64(to)
+				}
+			}
+		}
+	}
 }
